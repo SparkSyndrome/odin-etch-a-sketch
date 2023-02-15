745e81a352df13bfed92e9a19a02f26764a4d550
Christopher Burton <christopherburton1020@gmail.com>
2023-02-14 17:19:50 -0700

    Add functionality to change grid size

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-const gridContainer = document.getElementById('grid-container');
-let gridCell;
-let oldGridLength;
+makeGrid(16);
 
-function makeGrid(rows, cols) {
-  oldGridLength = (rows * cols);
-  console.log(oldGridLength);
-  gridContainer.style.setProperty('--grid-rows', rows);
-  gridContainer.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
-    gridCell = document.createElement('div');
+function makeGrid(gridSize) {
+  const gridContainer = document.getElementById('grid-container');
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  let numDivs = (gridSize * gridSize);
+  for (let i = 0; i < numDivs; i++) {
+    let gridCell = document.createElement('div');
     gridContainer.appendChild(gridCell).className = 'grid-cell';
     gridCell.addEventListener('mouseover', (e) => e.target.classList.add('active'));
   }
 }
 
-makeGrid(16, 16);
+const selectGridSizeBtn = document.getElementById('select');
+
+function getNewGridSize() {
+  let userInput = prompt("Enter your desired grid size (up to 100): ");
+  if (userInput > 0 && userInput <= 100) {
+    makeGrid(userInput);
+  } else {
+    getNewGridSize();
+  }
+}
+
+selectGridSizeBtn.addEventListener('click', getNewGridSize);
